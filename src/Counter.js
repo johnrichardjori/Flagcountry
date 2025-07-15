a766f48f548ba393d314c2591ee913df8c127c54
johnrichardjori <johnrichardjori123@gmail.com>
2025-07-15 10:16:44 +0530

    updated

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import './Counter.css';
+
+function Counter() {
+  const [Count, setCount] = useState(0);
+
+  const Increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const Decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <>
+      {Count < 0 ? <p>0</p> : <p>{Count}</p>}
+
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+    </>
+  );
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Flag from './flag';
 // import LocationSelector from './States';
-import FullNameForm from './Fullname';
+// import FullNameForm from './Fullname';
+import Counter from './Counter';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FullNameForm />
+    <Counter />
   </React.StrictMode>
 );
 
