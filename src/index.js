fd6cb140edec6985b4b037ce18fd48ddbe49c326
johnrichardjori <johnrichardjori123@gmail.com>
2025-07-15 10:05:40 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Flag from './flag';
-import LocationSelector from './States';
+// import LocationSelector from './States';
+import FullNameForm from './Fullname';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LocationSelector />
+    <FullNameForm />
   </React.StrictMode>
 );
 
