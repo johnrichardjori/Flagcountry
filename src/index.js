9c7a0f7a06489a706cfcfb4a0c46a93457fad187
johnrichardjori <johnrichardjori123@gmail.com>
2025-07-15 09:53:13 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Flag from './flag';
+// import Flag from './flag';
+import LocationSelector from './States';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Flag />
+    <LocationSelector />
   </React.StrictMode>
 );
 
